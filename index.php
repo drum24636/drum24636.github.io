f876c8ecda6a301200f4ae493cb788001369c6ac
Unknown <drum.rw60@gmail.com>
2017-06-14 17:56:09 +0700

    start page

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+    <script src="js/jquery-3.2.1.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <style media="screen">
+    body {
+      background-image : url('img/maxresdefault.jpg');
+      background-repeat : no-repeat;
+      background-position : center top;
+      background-color : #93c3f7;
+      margin-top: 0px;
+      background-attachment:fixed;
+      font: 0.8em sans-serif;
+      color: #333333;
+    }
+    .block {
+      opacity: 0.5;
+      margin:auto;
+    }
+    .block:hover {
+      opacity: 1.0;
+    }
+    .block2 {
+      opacity: 0.9;
+      
+    }    
+    .grow:hover {
+      transform: scale(1.5);
+      transition-duration: 3s;
+      z-index: 1050;
+    }
+    </style>
+  </head>
+  <body>
+    <div class="container" style="margin-top:5%;">
+      <div class="row">
+        <div class="offset-1 col-3">
+          <img class="rounded-circle grow" src="img/profile.jpg" width="200px" height="200px" style="border:5px dotted orange;">
+        </div>
+        <div class="card col-6" style="opacity:0.8">
+          <div class="card-block">
+            <h2 class="card-title">สรุธัช&nbsp;&nbsp;มิ่งแก้ว</h2><br>
+            <h6 class="card-subtitle mb-2 text-muted">line ID: dump01</h6><br>
+            <h6 class="card-subtitle mb-2 text-muted">phone: 0-2942-8445 ต่อ 406</h6><br>
+            <h6 class="card-subtitle mb-2 text-muted">email: fgrasum@ku.ac.th</h6>
+            <img class="card-img-bottom">
+          </div>
+        </div>
+      </div>
+      <div class="row" style="margin-top:5%;">
+<!--        <div class="col-12">-->
+         <div class="card-group">
+            <div class="card block2">
+              <img class="card-img-top" src="img/birthplace.jpg" alt="Card image cap" width="253" height="180" style="margin:auto;">
+              <div class="card-block ">
+                <h4 class="card-title">สถานที่เกิด</h4>
+                <p class="card-text">โรงพยาบาลเซนต์หลุยส์ <br>27 ถนนสาทรใต้ แขวงยานนาวา <br>เขตสาทร กรุงเทพฯ 10120<br><br>เมื่อ 28 เมษายน 2536 เวลา 18.35 น.</p>
+              </div>
+            </div>
+            <div class="card block2">
+              <img class="card-img-top" src="img/highschool.jpg" alt="Card image cap" width="253" height="180" style="margin:auto;">
+              <div class="card-block ">
+                <h4 class="card-title">มัธยมศึกษา</h4>
+                <p class="card-text">โรงเรียนฤทธิยะวรรณาลัย <br>171/3151 ถนนพหลโยธิน <br>แขวงคลองถนน เขตสายไหม <br>กรุงเทพฯ 10220<br><br>ตั้งแต่ พ.ศ. 2548 - 2554</p>
+
+              </div>
+            </div>
+            <div class="card block2">
+              <img class="card-img-top" src="img/university.jpg" alt="Card image cap" width="253" height="180" style="margin:auto;">
+              <div class="card-block ">
+                <h4 class="card-title">อุดมศึกษา</h4>
+                <p class="card-text">คณะวิศวกรรมศาสตร์ศรีราชา มหาวิทยาลัยเกษตรศาสตร์ <br>วิทยาเขตศรีราชา <br>199 ถนนสุขุมวิท ตำบลทุ่งสุขลา <br>อำเภอศรีราชา ชลบุรี 20230<br><br>ตั้งแต่ พ.ศ. 2554 - 2558</p>
+
+              </div>
+            </div>
+            <div class="card block2">
+              <img class="card-img-top" src="img/workplace.jpg" alt="Card image cap" width="253" height="180" style="margin:auto;">
+              <div class="card-block ">
+                <h4 class="card-title">สถานที่ทำงาน</h4>
+                <p class="card-text">บัณฑิตวิทยาลัย <br>มหาวิทยาลัยเกษตรศาสตร์ <br>50 ถนนพหลโยธิน แขวงลาดยาว <br>เขตจตุจักร กรุงเทพฯ 10903 <br><br>ตั้งแต่ พ.ศ. 2558 - ปัจจุบัน</p>
+
+              </div>
+            </div>
+          
+         </div>
+<!--        </div>-->
+      </div>
+    </div>
+  </body>
+</html>
